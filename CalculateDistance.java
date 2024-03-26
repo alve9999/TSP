9a3c99476b96927fc362b0ccb481e8028f7aed70
alve9999 <alve.lindell@gmail.com>
2024-03-26 10:13:09 +0100

    fix

--- CalculateDistance.java
+++ CalculateDistance.java
@@ -2,12 +2,12 @@ public class CalculateDistance {
     public int calculateTourLength(double[][] connections,int[] tour) {
         int length = 0;
         for (int i = 0; i < tour.length - 1; i++) {
-            int from = tour.get(i);
-            int to = tour.get(i + 1);
+            int from = tour[i];
+            int to = tour[i + 1];
             length += connections[from][to];
         }
         // Add length from the last city back to the starting city
-        length += connections[tour.get(tour.size() - 1)][tour.get(0)];
+        length += connections[tour[tour.length - 1]][tour[0]];
         return length;
     }
 }
