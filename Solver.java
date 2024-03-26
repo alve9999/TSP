7fd1bc0233dde3868e811e930735d4e1394bc7b2
alve9999 <alve.lindell@gmail.com>
2024-03-26 09:49:41 +0100

    inteface

---
+++ Solver.java
@@ -0,0 +1,13 @@
+
+
+public interface TSPSolver {
+    /**
+     * Solves the Traveling Salesman Problem (TSP) given a matrix of distances
+     * between points and an array of points.
+     * 
+     * @param distances The matrix of distances between points.
+     * @param points    The array of points.
+     * @return An array representing the optimal order of visiting the points.
+     */
+    int[] solveTSP(double[][] distances, Point[] points);
+}
