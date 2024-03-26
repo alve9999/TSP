37bf50f294166797bd4ae0150858583655f028a6
alve9999 <alve.lindell@gmail.com>
2024-03-26 09:43:45 +0100

    point matrix

--- TSP.java
+++ TSP.java
@@ -1,6 +1,39 @@
 import javax.swing.*;
 import java.awt.*;
 import java.util.Random;
+import java.lang.Math;
+
+class PointMatrix {
+    private double[][] matrix;
+
+    public PointMatrix(Point[] points) {
+        matrix = new double[points.length][points.length];
+        for (int i = 0; i < points.length; i++) {
+            for (int j = 0; j < points.length; j++) {
+                if (i != j) {
+		    double dx = (points[i].x-points[i].x);
+		    double dy = (points[i].y-points[j].y);
+                    matrix[i][j] = Math.sqrt(dx*dx+dy*dy);
+                } else {
+                    matrix[i][j] = 0.0;
+                }
+            }
+        }
+    }
+
+    public double[][] getMatrix() {
+        return matrix;
+    }
+
+    public void printMatrix() {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.printf("%.2f\t", matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
 
 public class TSP extends JPanel {
 
@@ -8,14 +41,16 @@ public class TSP extends JPanel {
     private static final int HEIGHT = 1000;
     private static final int POINT_SIZE = 5;
     private static final int NUM_POINTS = 100;
-
+    private PointMatrix connections;
+    
     private Point[] points;
-
+    
     public TSP() {
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         generateRandomPoints();
+	connections = new PointMatrix(points);
     }
-
+    
     private void generateRandomPoints() {
         points = new Point[NUM_POINTS];
         Random random = new Random();
@@ -30,7 +65,7 @@ public class TSP extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         for (Point point : points) {
-            g.setColor(Color.RED);
+            g.setColor(Color.BLACK);
             g.fillOval(point.x, point.y, POINT_SIZE, POINT_SIZE);
         }
     }
@@ -39,8 +74,12 @@ public class TSP extends JPanel {
         JFrame frame = new JFrame("Random Points Display");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(new TSP());
-        frame.pack();
+
+
+	frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 }
+ 
+ 
