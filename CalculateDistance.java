c99da7b16b310111e9b575a7e4f302600c2e724f
alve9999 <alve.lindell@gmail.com>
2024-03-26 10:09:33 +0100

    calculate distance

---
+++ CalculateDistance.java
@@ -0,0 +1,13 @@
+public class CalculateDistance {
+    public int calculateTourLength(double[][] connections,int[] tour) {
+        int length = 0;
+        for (int i = 0; i < tour.size() - 1; i++) {
+            int from = tour.get(i);
+            int to = tour.get(i + 1);
+            length += connections[from][to];
+        }
+        // Add length from the last city back to the starting city
+        length += connections[tour.get(tour.size() - 1)][tour.get(0)];
+        return length;
+    }
+}
